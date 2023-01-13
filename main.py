994b3aede08fbb67e39244f994df460787e221f8
bglb <bglb@qq.com>
2023-01-14 02:17:34 +0800

    fix: 修复gitflow下的编译

--- main.py
+++ main.py
@@ -2,6 +2,6 @@
 # @Time     : 2022/8/4 22:38
 # @Author   : BGLB
 # @Software : PyCharm
-from core import main
 from package_import import * # 必须导入 由于编译成了 pyd 所以 pyinstalle 找不到pyd里面的包
+from core import main
 main()
